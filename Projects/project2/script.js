25bc85e0160451e47c03c8376f49ed05ff02eb82
he-rro <hero.0073722gmail.com>
2022-06-29 04:54:00 -0700

    complet project section in index.html

--- Projects/project2/script.js
+++ Projects/project2/script.js
@@ -1,40 +1,83 @@
 
-let num1;
-let num2;
-let result=0;
-let num1El=document.getElementById('num1El');
-let num2El=document.getElementById('num2El');
-let resultEl=document.getElementById('resultEl');
-let errorEl =document.getElementById('error');
+let cards=[];
+let sumOfCards=0;
+let hasBlackjack=false;
+let isAlive=false;
+let message="";
+let messageEl=document.getElementById('messageEl');
+let cardEl=document.getElementById('cardEl');
+let sumEl=document.querySelector('#sumEl');
 
-function genarateNum(){
-    num1=Math.floor((Math.random() * 1000) )
-   num2=Math.floor((Math.random() * 1000) )
-   num1El.textContent=num1;
-    num2El.textContent=num2;
+function renderGame(){
+
+cardEl.textContent ='Cards : ';
+   for(let i =0 ;i<cards.length ;i ++){  
+ cardEl.textContent +=' '+cards[i];
   }
+
+  sumEl.textContent= 'Sum :  '+sumOfCards;
+  if(sumOfCards<=20){
+    message="Add new Card";
+  }
+  else if(sumOfCards===21){
+    message="you've got blackJack";
+    hasBlackjack=true;
+  }
+  else{
+    message="you are out Of The Game ";
+    isAlive=false;
+  }
+  messageEl.textContent=message;
   
-function add(){
-   genarateNum();
-result=num1+num2;
-resultEl.innerText='sum : '+result;
 }
 
-function subtract(){
-  genarateNum()
-  result=num1-num2;
-  resultEl.textContent='sub : '+result;
+ function startGame(){
+  isAlive=true
+  let firstCard=randomCard();
+let secondCard=randomCard();
+ cards=[firstCard,secondCard]
+ sumOfCards=firstCard+secondCard;
+  renderGame();
+}
+
+function newCard(){
+  if(isAlive===true && hasBlackjack===false){
+    let addNewCards=Math.floor((Math.random() * 13) + 1);
+    cards.push(addNewCards);
+   sumOfCards +=addNewCards;
+   renderGame();
+  }else{
+    message="Game Over ,Click Start Game ";
+    messageEl.textContent=message;
   }
 
-function multiply(){
-    genarateNum()
-    result=num1*num2;
-    resultEl.textContent='mul : '+result;
-    }
+ 
+}
 
-function divide(){
-    genarateNum()
-      result=num1/num2;
-      resultEl.textContent='div : '+result;
-      }
+function randomCard(){
+let number=Math.floor((Math.random() * 13) + 1);
+if(number===1){
+  return 1;
+}
+if (number>10) {
+  return 10;
+} 
+else{
+  return number;
+}
+
+}
+
+
+
+
+
+
+/* //for Loop
+let greatings=['hey ','my name is ','hero'];
+let greatId=document.getElementById('greatId');
+for(let g=0 ; g<greatings.length ;g ++){ 
+  console.log(greatings[g]); 
+  greatId.textContent += greatings[g];
+} */
 
